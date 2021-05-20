6a890d72d43337195b101f50241baa55521d4205
Petra <petra@NB-PETRA.localdomain>
2021-05-20 15:42:40 +0100

    node http server + React client added

---
+++ http_server/index.js
@@ -0,0 +1,17 @@
+//import Mongo from 'mongodb';
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the server!"});
+});
+
+app.listen(PORT, () => {
+  console.log("Browser server listening on " + PORT);
+})
