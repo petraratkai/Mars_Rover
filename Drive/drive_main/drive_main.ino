efaac2e820b3bacf62026eee986a84396a6a0306
Miles Grist <milesgrist@gmail.com>
2021-05-26 18:32:56 +0100

    OOP abstraction of smps code

--- Drive/drive_main/drive_main.ino
+++ Drive/drive_main/drive_main.ino
@@ -17,26 +17,11 @@
 
 #include "drive_motor.hpp"
 #include "drive_ofs.hpp"
+#include "drive_smps.hpp"
 
 
 //************************** SMPS **************************//
 INA219_WE ina219; // this is the instantiation of the library for the current sensor
-
-float open_loop, closed_loop; // Duty Cycles
-float vpd,vb,iL,dutyref,current_mA; // Measurement Variables
-float vref = 2;
-unsigned int sensorValue0,sensorValue1,sensorValue2,sensorValue3;  // ADC sample values declaration
-float ev=0,cv=0,ei=0,oc=0; //internal signals
-float Ts=0.0008; //1.25 kHz control frequency. It's better to design the control period as integral multiple of switching period.
-float kpv=0.05024,kiv=15.78,kdv=0; // voltage pid.
-float u0v,u1v,delta_uv,e0v,e1v,e2v; // u->output; e->error; 0->this time; 1->last time; 2->last last time
-float kpi=0.02512,kii=39.4,kdi=0; // current pid.
-float u0i,u1i,delta_ui,e0i,e1i,e2i; // Internal values for the current controller
-float uv_max=4, uv_min=0; //anti-windup limitation
-float ui_max=1, ui_min=0; //anti-windup limitation
-float current_limit = 1.0;
-boolean Boost_mode = 0;
-boolean CL_mode = 1;
 
 unsigned int loopTrigger;
 unsigned int com_count=0;   // a variables to count the interrupts. Used for program debugging.
@@ -62,6 +47,7 @@ int target_x_change = 0; // in pixels, converted from the target angle
 
 drive_motor motor;
 drive_ofs ofs;
+drive_smps smps;
 //**********************************************************************//
 
 
@@ -113,7 +99,7 @@ void setup() {
   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP1_bm; 
 
-  // TimerB0 initialization for PWM output
+  // TimerB0 initialization for SMPS PWM output
   
   pinMode(6, OUTPUT);
   TCB0.CTRLA=TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm; //62.5kHz
@@ -128,19 +114,13 @@ void setup() {
  
 bool t = true;
 bool t2 = true;
+
 void loop() {
-  unsigned long currentMillis = millis();
-  if(loopTrigger) { // This loop is triggered, it wont run unless there is an interrupt
+  unsigned long currentMillis = millis(); // Slower due to TCA0?
+  if(loopTrigger) { // 1000/1024 kHz, set by TCA0
     loopTrigger = 0;
-    sampling();
-    current_limit = 3; // Buck has a higher current limit
-    ev = vref - vb;  //voltage error at this time
-    cv=pidv(ev);  //voltage pid
-    cv=saturation(cv, current_limit, 0); //current demand saturation
-    ei=cv-iL; //current error
-    closed_loop=pidi(ei);  //current pid
-    closed_loop=saturation(closed_loop,0.99,0.01);  //duty_cycle saturation
-    pwm_modulate(closed_loop); //pwm modulation
+    
+    smps.update();
 
     // ------------Update OFS ---------------//
     MD md;
@@ -149,17 +129,17 @@ void loop() {
 
     switch(current_command_state){
       case rover_standby:
-        vref = 1;
+        smps.vref = 1;
         break;
       case rover_move:
-        vref = 1.5 + 0.005*(target_dist - ofs.total_y); // reduced accuracy
+        smps.vref = 1.5 + 0.005*(target_dist - ofs.total_y); // reduced accuracy
         motor.setMotorDelta(5*ofs.total_x1);
         if ((target_dist - ofs.total_y) < 1){
           roverStandby();
         }
         break;
       case rover_rotate:
-        vref = 1.5 + 0.005*(target_x_change - ofs.total_x1);
+        smps.vref = 1.5 + 0.005*(target_x_change - ofs.total_x1);
         motor.setMotorDelta(5*ofs.total_y1);
         if ((target_x_change - ofs.total_x1) < 1){
           roverStandby();
@@ -214,105 +194,6 @@ void loop() {
 ISR(TCA0_CMP1_vect){
   TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP1_bm; //clear interrupt flag
   loopTrigger = 1;
-}
-
-// This subroutine processes all of the analogue samples, creating the required values for the main loop
-
-void sampling(){
-
-  // Make the initial sampling operations for the circuit measurements
-  
-  sensorValue0 = analogRead(A0); //sample Vb
-  sensorValue2 = analogRead(A2); //sample Vref
-  sensorValue3 = analogRead(A3); //sample Vpd
-  current_mA = ina219.getCurrent_mA(); // sample the inductor current (via the sensor chip)
-
-  // Process the values so they are a bit more usable/readable
-  // The analogRead process gives a value between 0 and 1023 
-  // representing a voltage between 0 and the analogue reference which is 4.096V
-  
-  vb = sensorValue0 * (4.096 / 1023.0); // Convert the Vb sensor reading to volts
-  //vref = sensorValue2 * (4.096 / 1023.0); // Convert the Vref sensor reading to volts
-  //vref = 4;
-  vpd = sensorValue3 * (4.096 / 1023.0); // Convert the Vpd sensor reading to volts
-
-  // The inductor current is in mA from the sensor so we need to convert to amps.
-  // We want to treat it as an input current in the Boost, so its also inverted
-  // For open loop control the duty cycle reference is calculated from the sensor
-  // differently from the Vref, this time scaled between zero and 1.
-  // The boost duty cycle needs to be saturated with a 0.33 minimum to prevent high output voltages
-  
-  if (Boost_mode == 1){
-    iL = -current_mA/1000.0;
-    dutyref = saturation(sensorValue2 * (1.0 / 1023.0),0.99,0.33);
-  }else{
-    iL = current_mA/1000.0;
-    dutyref = sensorValue2 * (1.0 / 1023.0);
-  }
-  
-}
-
-float saturation( float sat_input, float uplim, float lowlim){ // Saturatio function
-  if (sat_input > uplim) sat_input=uplim;
-  else if (sat_input < lowlim ) sat_input=lowlim;
-  else;
-  return sat_input;
-}
-
-void pwm_modulate(float pwm_input){ // PWM function
-  analogWrite(6,(int)(255-pwm_input*255)); 
-}
-
-// This is a PID controller for the voltage
-
-float pidv( float pid_input){
-  float e_integration;
-  e0v = pid_input;
-  e_integration = e0v;
- 
-  //anti-windup, if last-time pid output reaches the limitation, this time there won't be any intergrations.
-  if(u1v >= uv_max) {
-    e_integration = 0;
-  } else if (u1v <= uv_min) {
-    e_integration = 0;
-  }
-
-  delta_uv = kpv*(e0v-e1v) + kiv*Ts*e_integration + kdv/Ts*(e0v-2*e1v+e2v); //incremental PID programming avoids integrations.there is another PID program called positional PID.
-  u0v = u1v + delta_uv;  //this time's control output
-
-  //output limitation
-  saturation(u0v,uv_max,uv_min);
-  
-  u1v = u0v; //update last time's control output
-  e2v = e1v; //update last last time's error
-  e1v = e0v; // update last time's error
-  return u0v;
-}
-
-// This is a PID controller for the current
-
-float pidi(float pid_input){
-  float e_integration;
-  e0i = pid_input;
-  e_integration=e0i;
-  
-  //anti-windup
-  if(u1i >= ui_max){
-    e_integration = 0;
-  } else if (u1i <= ui_min) {
-    e_integration = 0;
-  }
-  
-  delta_ui = kpi*(e0i-e1i) + kii*Ts*e_integration + kdi/Ts*(e0i-2*e1i+e2i); //incremental PID programming avoids integrations.
-  u0i = u1i + delta_ui;  //this time's control output
-
-  //output limitation
-  saturation(u0i,ui_max,ui_min);
-  
-  u1i = u0i; //update last time's control output
-  e2i = e1i; //update last last time's error
-  e1i = e0i; // update last time's error
-  return u0i;
 }
 
 //************************* Rover Commands ************************//
---
+++ Drive/drive_main/drive_smps.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Arduino.h>
+#include <INA219_WE.h>
+
+class drive_smps {
+    INA219_WE ina219; // this is the instantiation of the library for the current sensor
+
+    float open_loop, closed_loop; // Duty Cycles
+    float vpd, vb, iL, dutyref, current_mA; // Measurement Variables
+
+    unsigned int sensorValue0, sensorValue1, sensorValue2, sensorValue3;  // ADC sample values declaration
+    float ev=0,cv=0,ei=0,oc=0; //internal signals
+
+    const float Ts=0.0008; //1.25 kHz control frequency. It's better to design the control period as integral multiple of switching period.
+    const float kpv=0.05024,kiv=15.78,kdv=0; // voltage pid.
+
+    float u0v, u1v, delta_uv, e0v, e1v, e2v; // u->output; e->error; 0->this time; 1->last time; 2->last last time
+
+    const float kpi=0.02512, kii=39.4, kdi=0; // current pid.
+
+    float u0i, u1i, delta_ui, e0i, e1i, e2i; // Internal values for the current controller
+
+    const float uv_max= 4, uv_min=0; //anti-windup limitation
+    const float ui_max= 1, ui_min=0; //anti-windup limitation
+    const float current_limit = 3.0;
+
+    const boolean Boost_mode = 0;
+    const boolean CL_mode = 1;
+
+    void sampling();
+    float saturation( float sat_input, float uplim, float lowlim);
+    void pwm_modulate(float pwm_input);
+    float pidv( float pid_input);
+    float pidi(float pid_input);
+
+public:
+    float vref = 2;
+    
+    void update();
+};
