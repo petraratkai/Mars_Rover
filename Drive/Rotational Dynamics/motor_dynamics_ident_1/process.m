5ddfd66d06b7bd56bd2fbced13fb06e10c20906f
Miles Grist <milesgrist@gmail.com>
2021-05-31 14:18:16 +0100

    dx modelling

--- Drive/Rotational Dynamics/motor_dynamics_ident_1/process.m
+++ Drive/Rotational Dynamics/motor_dynamics_ident_1/process.m
@@ -2,27 +2,27 @@ clear
 close all
 clc
 
-D = readmatrix("test2.csv");
-pos_y = D(:, 2).';
-v = [4*ones(1, 108), zeros(1, 945)]; 
+D = readmatrix("test1.csv");
+pos_x = D(:, 1).';
+v = [4*ones(1, 108), zeros(1, 967)]; 
 t = D(:, 3).';%.*1.024;
-dy_dt = gradient(pos_y, t);
-dy_mean = movmean(dy_dt, 30); % approximately pixels/ms
+dx_dt = gradient(pos_x, t);
+dx_mean = movmean(dx_dt, 30); % approximately pixels/ms
 
 figure
 hold on
-plot(t, pos_y);
+plot(t, pos_x);
 hold off
 
 figure
 hold on
-plot(t, dy_dt);
+plot(t, dx_dt);
 plot(t, v);
 hold off
 
 figure
 hold on
-plot(t, dy_mean);
+plot(t, dx_mean);
 plot(t, v);
 xlim([1000 1800]);
 hold off
--- Drive/Rotational Dynamics/motor_dynamics_ident_2/process.m
+++ Drive/Rotational Dynamics/motor_dynamics_ident_2/process.m
@@ -2,37 +2,37 @@ clear
 close all
 clc
 
-zeta = 0.7;
-w_n = 30;
-sys = tf(624, [1, 2*zeta*w_n, w_n^2]);
+zeta = 1;
+w_n = 45;
+sys = tf(2000, [1, 2*zeta*w_n, w_n^2]);
 
-D = readmatrix("test1_forced.csv");
-pos_y = D(:, 2).';
-v = [4*ones(1, 268), zeros(1, 4), -4*ones(1, 266), zeros(1, 532)]; 
+D = readmatrix("test2.csv");
+pos_x = D(:, 1).';
+v = [4*ones(1, 268), zeros(1, 4), -4*ones(1, 266), zeros(1, 511)]; 
 t = D(:, 3).';%.*1.024;
-dy_dt = gradient(pos_y, t);
-dy_mean = movmean(dy_dt, 30); % approximately pixels/ms
+dx_dt = gradient(pos_x, t);
+dx_mean = movmean(dx_dt, 30); % approximately pixels/ms
 
 figure
 hold on
-plot(t, pos_y);
+plot(t, pos_x);
 hold off
 
 figure
 hold on
-plot(t, dy_dt);
+plot(t, dx_dt);
 plot(t, v);
 hold off
 
 vsim = [4*ones(1, 500), zeros(1, 10), -4*ones(1, 500), zeros(1, 990)];
 tsim = 1:1e-3:2.999;
-y = lsim(sys, vsim, tsim);
+x = lsim(sys, vsim, tsim);
 
 figure
 hold on
-plot(t, dy_mean);
+plot(t, dx_mean);
 plot(t, v);
-plot(tsim*1000, y);
+plot(tsim*1000, x);
 xlabel("cycles")
 ylabel("dy/cycle")
 hold off
