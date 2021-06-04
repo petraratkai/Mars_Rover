c9e43274cb9fe78cb3b314c4909a9f351a4fed10
Miles Grist <milesgrist@gmail.com>
2021-06-04 15:24:18 +0100

    setup power test program

---
+++ Drive/drive_power_test/drive_power_test.ino
@@ -0,0 +1,308 @@
+/*
+ * Original SMPS Program written by Yue Zhu (yue.zhu18@imperial.ac.uk) in July 2020.
+ * pin6 is PWM output at 62.5kHz.
+ * duty-cycle saturation is set as 2% - 98%
+ * Control frequency is set as 1.25kHz. 
+ * 
+ * This program is an adaptation of Yue Zhu's work, combined with the sample code provided for the EE2 Rover.
+ * Author, Miles Grist
+ * Date, May 2021
+ * 
+ * This sketch is for an Arduino Nano Every to drive a SMPS, dual H-bridge motor driver and optical flow sensor.
+*/
+
+#include "SPI.h"
+#include <Wire.h>
+#include <INA219_WE.h>
+
+#include "drive_motor.hpp"
+#include "drive_ofs.hpp"
+#include "drive_smps.hpp"
+
+
+//************************** SMPS **************************//
+INA219_WE ina219; // this is the instantiation of the library for the current sensor
+
+unsigned int loopTrigger;
+unsigned int com_count=0;   // a variables to count the interrupts. Used for program debugging.
+//*********************************************************//
+
+//************************ Control Variables ***************************//
+enum command_state {rover_standby, rover_move, rover_rotate, rover_stop};
+
+command_state current_command_state = rover_standby;
+float target_dist = 0; // in mm
+long int target_pixel_dist = 0;
+float target_angle = 0; // in degrees
+int target_x_pixel_change = 0; // in pixels, converted from the target angle
+
+// PID
+float e1 = 0; // dy controller
+float acc1 = 0;
+
+float e2 = 0;
+float acc2 = 0; // dx controller
+
+const float y_kp = 0.003; // P gain of the y controller (forward distance)
+const float x_kp = 0.004; // P gain of the x controller (angle)
+
+float max_vref = 4;
+
+const short int endpoint_beta = 3; // Tolerance of commands at the endpoint before returning to standby
+const short int endpoint_time_delta = 100; // Time (in control loop cycles) for which the endpoint needs to be reached before standby
+short int endpoint_cycles_elapsed = 0;
+
+short int stop_cycles_elapsed = 0; // Timer. Wait 'endpoint_cycles_elapsed' before returning errors via control link, after the rover has received a stop command
+bool return_error_due = false;
+bool return_success_due = false;
+
+
+drive_motor motor;
+drive_ofs ofs;
+drive_smps smps;
+//**********************************************************************//
+
+// Control link
+const byte received_data_length = 32;
+char received_data[received_data_length];
+char temp_data[received_data_length];
+bool data_available = false;
+String input_string = "";
+
+char command_from_control[10] = {0};
+float float_from_control = 0; 
+
+
+void setup() {
+  smps.setup();
+  motor.setup();     
+  ofs.setup();
+
+  //Serial1.begin(9600); // UART connection for the control link
+  Serial.begin(9600);
+}
+ 
+bool t = true;
+bool t2 = true;
+
+void loop() {
+  unsigned long currentMillis = millis(); // Slower due to TCA0
+
+  readToBuffer();
+  
+  if(loopTrigger) { // 1000/1024 kHz, set by TCA0
+    loopTrigger = 0;
+    
+    smps.update(); // ------------- Update SMPS ------------//
+
+    MD md; // ------------Update OFS ---------------//
+    ofs.update(&md);
+
+    const float Vt = 1; // Minimum voltage for motor rotation
+    float received_float = 0;
+    String incoming_command = "";
+    
+    switch(current_command_state){
+      case rover_standby:
+        smps.vref = max_vref;
+        if (stop_cycles_elapsed < endpoint_time_delta){ // Delay progression of the state machine to give time for the rover to stop moving
+          stop_cycles_elapsed++;
+        }
+        else{
+          if(return_error_due){
+            Serial.println("driveFail");
+            Serial.println((target_pixel_dist - ofs.total_y1)/(15.748f)); // Send the error in mm from the expected endpoint
+            Serial.println((target_x_pixel_change - ofs.total_x1)/(38.0f)); // Send the error in degrees
+            return_error_due = false;
+          }
+          if(return_success_due){
+            Serial.println("driveDone"); // Lets the control system know the rover is in standby and available for new commands
+            return_success_due = false;
+          }
+
+          if(data_available){
+            Serial.println("data received");
+            strcpy(temp_data, received_data);
+            parseCommand();
+            Serial.println(command_from_control);
+            Serial.println(float_from_control);
+
+            if (!strcmp(command_from_control, "rotate")){
+              roverRotate(float_from_control);
+            } else if (!strcmp(command_from_control, "drive")){
+              roverMove(float_from_control);
+            }
+            data_available = false;
+          }
+        }
+        break;
+
+      case rover_move:
+        if (roverUpdate()){
+          roverStandby();
+        }
+        checkStop();
+        Serial.print(smps.getiL());
+        Serial.print(",");
+        Serial.println(currentMillis);
+        break;
+
+      case rover_rotate:
+        if (roverUpdate()){
+          roverStandby();
+        }
+        checkStop();
+        break;
+
+      case rover_stop:
+        roverStandby();
+        break;
+
+      default:
+        roverStandby();
+        break;
+    }
+  }
+}
+
+
+// Timer A CMP1 interrupt. Every 800us the program enters this interrupt. 
+// This, clears the incoming interrupt flag and triggers the main loop.
+ISR(TCA0_CMP1_vect){
+  TCA0.SINGLE.INTFLAGS |= TCA_SINGLE_CMP1_bm; //clear interrupt flag
+  loopTrigger = 1;
+}
+
+//************************* Rover Commands ************************//
+
+// Switches the drive system to the rover_standby state
+void roverStandby(){
+  current_command_state = rover_standby;
+  target_dist = 0;
+  target_angle = 0;
+  motor.stopMotors();
+  //ofs.clear();
+}
+
+// Switches the drive system to the rover_move state
+void roverMove(float dist){
+  current_command_state = rover_move;
+  target_dist = dist;
+  target_pixel_dist = (long int)(dist*15.748);
+  target_x_pixel_change = 0;
+  if (dist > 0){
+    motor.setMotorDirection(fwd);
+  } else {
+    motor.setMotorDirection(bck);
+  }
+  ofs.clear();
+}
+
+// Switches the drive system to the rover_rotate state
+void roverRotate(float angle){
+  current_command_state = rover_rotate;
+  target_dist = 0;
+  target_pixel_dist = 0;
+  target_angle = angle;
+  target_x_pixel_change = (int)(angle*40.0f);
+  if (angle > 0){
+    motor.setMotorDirection(ccw);
+  } else {
+    motor.setMotorDirection(cw);
+  }
+  ofs.clear();
+}
+
+bool roverUpdate(){
+  float target_dy; // Intermediate PID values used in the control 
+  float target_dx;
+
+  float v1;
+  float v2;
+
+  smps.vref = max_vref;
+
+  long int y_diff = target_pixel_dist - ofs.total_y1;
+  long int x_diff = target_x_pixel_change - ofs.total_x1;
+
+  if (abs(y_diff) <= endpoint_beta){ // Check if the enpoint has been reached to a satisfactory accuracy
+    if (abs(x_diff) <= endpoint_beta){
+      if (endpoint_cycles_elapsed == endpoint_time_delta){ // Ensure endpoint has been met for a minimum time, avoid overshoot incorrectly identified as endpoint
+        endpoint_cycles_elapsed = 0;
+        return_success_due = true;
+        return true;
+      }
+      endpoint_cycles_elapsed++;
+    }
+  } else {
+    endpoint_cycles_elapsed = 0;
+  }
+        
+  target_dy = y_kp*(y_diff); // P controller for y
+  target_dx = x_kp*(x_diff); // P controller to maintain straight line
+
+  // Limit the speed setpoint to 3 pixels/cycle which is close to the maximum of the rover. Helps to avoid overshoot of endpoint (waste of power)
+  if (abs(target_dy) > 3){
+    target_dy = (target_dy > 0) ? 3 : -3;
+  }
+  if (abs(target_dx) > 3){
+    target_dx = (target_dx > 0) ? 3 : -3;
+  }
+
+  v1 = pid_update(ofs.getAvgdy(), target_dy, &e1, dykp, dyki, dykd, &acc1); // velocity PID controllers
+  v2 = pid_update(ofs.getAvgdx(), target_dx, &e2, dxkp, dxki, dxkd, &acc2); // Return value in volts 
+
+  motor.setMotorDelta((int)(v1/smps.vref*255), (int)(2*v2/smps.vref*255)); // Voltage setpoint is converted to a PWM value on the motors
+  motor.update();
+  return false;
+}
+
+//****************************************************************//
+
+//----------------------------- Control Connection -----------------------------//
+
+bool checkStop(){ // Checks serial buffer for the STOP instruction
+  if (data_available){
+    Serial.println("data received");
+    if(!strcmp(received_data, "stop")){
+      roverStandby();
+      return_error_due = true;
+      stop_cycles_elapsed = 0;
+    }
+    data_available = false;
+  }
+}
+
+void readToBuffer(){ // Read in new data from Serial1 to the received_data buffer
+  char end = '\n';
+  char c;
+  static short int i = 0;
+
+  while (Serial.available() > 0 && !data_available){
+    c = Serial.read();
+
+    if (c != end){
+      received_data[i] = c;
+      i = (i >= received_data_length - 1) ? i : i + 1;
+    } else {
+      received_data[i] = '\0'; // Null-terminated string
+      i = 0;
+      data_available = true;
+    }
+  }
+}
+
+void parseCommand(){ // Called during standby if there is data_available
+  char *strtoki;
+  strtoki = strtok(temp_data, ":");
+  strcpy(command_from_control, strtoki);
+  strtoki = strtok(NULL, "\0");
+  float_from_control = atof(strtoki); 
+}
+
+bool checkCurrentLimit(){
+  // CHECK FOR HIGH CURRENT (e.g. motors have been stopped) 
+  // return an error to control and move to standby
+  return false;
+}
+//------------------------------------------------------------------------------//
---
+++ Drive/drive_power_test/drive_smps.cpp
@@ -0,0 +1,137 @@
+#include "drive_smps.hpp"
+
+
+void drive_smps::sampling(){
+
+  // Make the initial sampling operations for the circuit measurements
+  
+  sensorValue0 = analogRead(A0); //sample Vb
+  sensorValue2 = analogRead(A2); //sample Vref
+  sensorValue3 = analogRead(A3); //sample Vpd
+  current_mA = ina219.getCurrent_mA(); // sample the inductor current (via the sensor chip)
+
+  // Process the values so they are a bit more usable/readable
+  // The analogRead process gives a value between 0 and 1023 
+  // representing a voltage between 0 and the analogue reference which is 4.096V
+  
+  vb = sensorValue0 * (4.096 / 1023.0); // Convert the Vb sensor reading to volts
+  vpd = sensorValue3 * (4.096 / 1023.0); // Convert the Vpd sensor reading to volts
+
+  // The inductor current is in mA from the sensor so we need to convert to amps.
+  // We want to treat it as an input current in the Boost, so its also inverted
+  // For open loop control the duty cycle reference is calculated from the sensor
+  // differently from the Vref, this time scaled between zero and 1.
+  // The boost duty cycle needs to be saturated with a 0.33 minimum to prevent high output voltages
+  
+  if (Boost_mode == 1){
+    iL = -current_mA/1000.0;
+    dutyref = saturation(sensorValue2 * (1.0 / 1023.0),0.99,0.33);
+  }else{
+    iL = current_mA/1000.0;
+    dutyref = sensorValue2 * (1.0 / 1023.0);
+  }
+  
+}
+
+float drive_smps::saturation( float sat_input, float uplim, float lowlim){ // Saturatio function
+  if (sat_input > uplim) sat_input=uplim;
+  else if (sat_input < lowlim ) sat_input=lowlim;
+  else;
+  return sat_input;
+}
+
+void drive_smps::pwm_modulate(float pwm_input){ // PWM function
+  analogWrite(6,(int)(255-pwm_input*255)); 
+}
+
+// This is a PID controller for the voltage
+
+float drive_smps::pidv( float pid_input){
+  float e_integration;
+  e0v = pid_input;
+  e_integration = e0v;
+ 
+  //anti-windup, if last-time pid output reaches the limitation, this time there won't be any intergrations.
+  if(u1v >= uv_max) {
+    e_integration = 0;
+  } else if (u1v <= uv_min) {
+    e_integration = 0;
+  }
+
+  delta_uv = kpv*(e0v-e1v) + kiv*Ts*e_integration + kdv/Ts*(e0v-2*e1v+e2v); //incremental PID programming avoids integrations.there is another PID program called positional PID.
+  u0v = u1v + delta_uv;  //this time's control output
+
+  //output limitation
+  saturation(u0v,uv_max,uv_min);
+  
+  u1v = u0v; //update last time's control output
+  e2v = e1v; //update last last time's error
+  e1v = e0v; // update last time's error
+  return u0v;
+}
+
+// This is a PID controller for the current
+
+float drive_smps::pidi(float pid_input){
+  float e_integration;
+  e0i = pid_input;
+  e_integration=e0i;
+  
+  //anti-windup
+  if(u1i >= ui_max){
+    e_integration = 0;
+  } else if (u1i <= ui_min) {
+    e_integration = 0;
+  }
+  
+  delta_ui = kpi*(e0i-e1i) + kii*Ts*e_integration + kdi/Ts*(e0i-2*e1i+e2i); //incremental PID programming avoids integrations.
+  u0i = u1i + delta_ui;  //this time's control output
+
+  //output limitation
+  saturation(u0i,ui_max,ui_min);
+  
+  u1i = u0i; //update last time's control output
+  e2i = e1i; //update last last time's error
+  e1i = e0i; // update last time's error
+  return u0i;
+}
+
+void drive_smps::update(){
+  sampling();
+  ev = vref - vb;  //voltage error at this time
+  cv=pidv(ev);  //voltage pid
+  cv=saturation(cv, current_limit, 0); //current demand saturation
+  ei=cv-iL; //current error
+  closed_loop=pidi(ei);  //current pid
+  closed_loop=saturation(closed_loop,0.99,0.01);  //duty_cycle saturation
+  pwm_modulate(closed_loop); //pwm modulation
+}
+
+void drive_smps::setup(){
+  noInterrupts(); //disable all interrupts
+  pinMode(3, INPUT_PULLUP); //Pin3 is the input from the Buck/Boost switch
+  pinMode(2, INPUT_PULLUP); // Pin 2 is the input from the CL/OL switch
+  analogReference(EXTERNAL); // We are using an external analogue reference for the ADC
+
+  // TimerA0 initialization for control-loop interrupt.
+  
+  TCA0.SINGLE.PER = 255;
+  TCA0.SINGLE.CMP1 = 255;
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;
+  TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP1_bm; 
+
+  // TimerB0 initialization for SMPS PWM output
+  
+  pinMode(6, OUTPUT);
+  TCB0.CTRLA=TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm; //62.5kHz
+  analogWrite(6, 120); 
+
+  interrupts();  //enable interrupts.
+  Wire.begin(); // We need this for the i2c comms for the current sensor
+  ina219.init(); // this initiates the current sensor
+  Wire.setClock(700000); // set the comms speed for i2c
+}
+
+float drive_smps::getiL(){
+  return iL;
+}
