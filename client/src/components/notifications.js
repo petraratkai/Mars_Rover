cdfe5144e8f0f7e7f7652cc00d5ade41d2b4e9af
Petra <petra@NB-PETRA.localdomain>
2021-06-05 23:12:29 +0100

    clear map button added

---
+++ client/src/components/notifications.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Notifications extends React.Component {
+  render () {
+    return (
+      <div>
+        hello
+        how are you
+        :)
+      </div>
+    );
+  }
+}
+
+export default Notifications;
