6d8662d2ac7600753aabe3c67d99132f6da6ddf4
Petra <petra@NB-PETRA.localdomain>
2021-06-09 16:23:10 +0100

    notifications

--- client/src/components/notifications.js
+++ client/src/components/notifications.js
@@ -3,7 +3,25 @@ import React, { Component } from 'react';
 class Notifications extends React.Component {
   render () {
     return (
-      <ul>
+      <ul className = "notifications">
+        <li>
+          hello
+        </li>
+        <li>
+          how are you
+        </li>
+        <li>
+          :)
+        </li>
+        <li>
+          hello
+        </li>
+        <li>
+          how are you
+        </li>
+        <li>
+          :)
+        </li>
         <li>
           hello
         </li>
