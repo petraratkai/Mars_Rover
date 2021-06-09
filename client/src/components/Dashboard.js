e60fb4466f25e2339153c7f98ea6e97d307607de
Petra <petra@NB-PETRA.localdomain>
2021-06-09 17:29:56 +0100

    notifications

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -35,7 +35,8 @@ class Dashboard extends Component {
                   <div className="legend">
                   <div className = "board">
                     <Map parentCallback = {this.handleCallback} style={{height: "100%"}} />
-                    </div>
+                  </div>
+                  <div className = "card-footer ">
                     <hr />
                     <div className ="stats">
                       <i className="fa fa-clock-o"></i>
