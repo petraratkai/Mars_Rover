c04b5e87a4463621a747fd1729ab0f125f21cba7
Petra <petra@NB-PETRA.localdomain>
2021-06-09 18:46:27 +0100

    notifications

--- client/src/components/notifications.js
+++ client/src/components/notifications.js
@@ -5,7 +5,7 @@ class Notifications extends React.Component {
     return (
       <div className = "not-wrapper">
       <div>
-      <button style={{float: "center"}}>
+      <button className = "clearmap">
         clear
       </button>
       </div>
