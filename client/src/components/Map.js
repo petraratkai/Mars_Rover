60c8c585c9862c6da3bb0a3f1c791468d757ec81
Petra <petra@NB-PETRA.localdomain>
2021-06-11 18:36:52 +0100

    dest images

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -131,7 +131,7 @@ class Map extends React.Component {
       x = x.toFixed(2);
       let y = (event.clientY-rect.top)/rect.height*100;
       y = y.toFixed(2);
-      this.props.parentCallback({x:1, y:(y-50)*(-1)});
+      this.props.parentCallback({x:x-50, y:(y-50)*(-1)});
     }
     }
   }
@@ -165,7 +165,7 @@ class Map extends React.Component {
           key={JSON.stringify(ball_el)}
           className="fa-stack the-wrapper ball"
           src = {ball.default} width = "3%"
-          style = {{top: ((ball_el.y-1.5)+50)*(-1)) + "%", left: (ball_el.x-1.5)+50 + "%"}}
+          style = {{top: ((ball_el.y-1.5)+50)*(-1) + "%", left: (ball_el.x-1.5)+50 + "%"}}
         />
       )
     })
