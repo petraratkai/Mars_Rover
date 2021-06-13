75aae485fa867ce843c26929563c4b37bbc3e17a
Petra <petra@NB-PETRA.localdomain>
2021-06-13 17:54:21 +0100

    dashboard aligned

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -64,7 +64,24 @@ class Dashboard extends Component {
                     </div>
                   </div>
                 </div>
-
+                <div className="col-md-4" >
+                  <div className="card">
+                    <div className="card-header ">
+                      <h4 className="card-title">Battery</h4>
+                      <p className="card-category">current state of battery:</p>
+                      </div>
+                      <div className="card-body ">
+                        <Battery/>
+                      </div>
+                    <div className="card-footer ">
+                      <hr />
+                      <div className="stats">
+                      <i className="fa fa-history"></i>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+              </div>
               <div className = "row" style={{height:"40%"}}>
                 <div className="card">
                   <div className="card-header ">
@@ -83,24 +100,6 @@ class Dashboard extends Component {
                 </div>
               </div>
             </div>
-            <div className="col-md-4" >
-              <div className="card">
-                <div className="card-header ">
-                  <h4 className="card-title">Battery</h4>
-                  <p className="card-category">current state of battery:</p>
-                  </div>
-                  <div className="card-body ">
-                    <Battery/>
-                  </div>
-                <div className="card-footer ">
-                  <hr />
-                  <div className="stats">
-                  <i className="fa fa-history"></i>
-                  </div>
-                </div>
-              </div>
-              </div>
-          </div>
 
           </div>
         </div>
