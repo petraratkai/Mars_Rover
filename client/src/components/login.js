4c9bffd56cd07fc901c23260d1228bbef2882064
Petra <petra@NB-PETRA.localdomain>
2021-06-13 23:44:20 +0100

    login

---
+++ client/src/components/login.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+
+async function loginUser(credentials) {
+ return fetch('http://localhost:8000/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
+export default function Login ({ setToken }) {
+  const [password, setPassword] = useState();
+  const handleSubmit = async e => {
+   e.preventDefault();
+   const token = await loginUser(
+     password
+   );
+   setToken(token);
+ }
+  render() {
+    return (
+      <div className = "login">
+        <h1>Please log in</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Password</p>
+            <input type="password" onChange={e => setPassword(e.target.value)}đ/>
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
