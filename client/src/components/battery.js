eb839cc8345154be2dc5ae3a28bb5080d305a695
Petra <petra@NB-PETRA.localdomain>
2021-06-13 16:07:34 +0100

    battery icon added

---
+++ client/src/components/battery.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Image from "react-bootstrap/Image"
+
+class Battery extends React.Component {
+
+
+  render() {
+    var battery = require('../assets/img/battery100.jpg');
+    return(
+      <Image
+
+          src = {battery.default} width = "100%%"
+      />
+    );
+  }
+}
