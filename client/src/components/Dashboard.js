36da31bcbb9c08037be1edc7894137a308f22e62
Petra <petra@NB-PETRA.localdomain>
2021-06-14 17:08:37 +0100

    button design + homepage

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -76,7 +76,7 @@ export default function Dashboard() {
                     <div>
                       <Clearmap/>
                     </div>
-
+                    
                   </div>
                 </div>
                 <div className="col-md-4" >
@@ -88,7 +88,11 @@ export default function Dashboard() {
                       <div className="card-body ">
                         <Battery/>
                       </div>
-
+                    <div className="card-footer ">
+                      <hr />
+                      <div className="stats">
+                      <i className="fa fa-history"></i>
+                      </div>
                     </div>
                   </div>
                   </div>
@@ -103,7 +107,12 @@ export default function Dashboard() {
                   <div className="card-body ">
                     <Notifications/>
                   </div>
-                  
+                  <div className="card-footer ">
+                    <hr />
+                    <div className="stats">
+                      <i className="fa fa-history"></i>
+                      </div>
+                  </div>
                 </div>
                 </div>
               </div>
