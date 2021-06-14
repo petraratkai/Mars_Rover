bbc69be0b3ebba61c6c23c37d75540e652a5b4f7
Petra <petra@NB-PETRA.localdomain>
2021-06-14 17:06:36 +0100

    button design + homepage

--- client/src/components/roverfeatures.js
+++ client/src/components/roverfeatures.js
@@ -8,7 +8,7 @@ export default class Roverfeatures extends React.Component {
         <li> Be sent to pre-set co-ordinates through the use of the map or (x,y) value </li>
         <li> Track movement of rover on the map </li>
         <li> Show obstacles on the map  </li>
-        <li> Find an optimal path from point A and B </li>
+        <li> Find an optimal path from point A to B </li>
         <li> Quick reaction to disturbance</li>
         <li> Evaluate when to stop the motors if there is an obstruction and thus conserve power</li>
         <li> Send battery status to server</li>
