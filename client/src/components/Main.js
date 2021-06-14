0ab7ce4dcf738a8d68307545ba044db6c8b8ecda
Petra <petra@NB-PETRA.localdomain>
2021-06-14 13:57:16 +0100

    homepage initisl

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,6 +4,7 @@ import Footer from './Footer'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import Other from './Other'
+import Home from './homepage.js'
 
 class Main extends Component {
   render() {
@@ -11,6 +12,7 @@ class Main extends Component {
       <div className="main-panel">
         <Navbar />
         <Switch>
+          <Route path = "/home" component = {Home} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/profile" component={Other} />
           <Redirect from='*' to='/dashboard' />
---
+++ client/src/components/homepage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className = "content">
+        <div className = "container-fluid">
+          <div className = "row">
+            Image of team
+          </div>
+          <div className = "row">
+            Team intro/about us
+          </div>
+          <div className = "row">
+            <div className = "col-md-6">
+              Rover features
+            </div>
+            <div className = "col-md-6">
+              Smth else
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+  }
+}
+
+export default Other;
