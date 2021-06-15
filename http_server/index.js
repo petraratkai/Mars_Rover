2a4056b7200c31ebe5a1368f64a8cb46fb024fcd
Petra <petra@NB-PETRA.localdomain>
2021-06-15 10:57:39 +0100

    references added

--- http_server/index.js
+++ http_server/index.js
@@ -12,17 +12,14 @@ var ball_coord = [];
 let ready = true;
 let roverWidth = 5;
 let safetyMargin = 5;
-
 var math = require('mathjs');
 let obst_complex = [];
 let allObstacles = [];
 let allHitboxes = [];
 let commandsNrOf = 0;
-
 let commands = [];
 let commands_complex = [];
 let path_complex = [];
-
 let notifications = [];
 //database stuff
 var dbo;
@@ -39,14 +36,10 @@ mongoClient.connect((err, db) => {
 	}
 	console.log("MongoDB connected")
 });
-
 function publish(topic,msg,options){
 console.log("publishing",msg);
-
 if (client.connected == true){
-
 client.publish(topic,msg,options);
-
 }
 count+=1;
 /*if (count==2) //ens script
@@ -54,176 +47,50 @@ count+=1;
 	client.end();
 }*/
 }
-
-
 //receive messages - listener
 client.on('message', (topic, message, packet) => {
 	console.log("message is "+ message);
 	console.log("topic is "+ topic);
 	if(topic == 'ready') {
 		var sort_chron = {time: 1};
-		if(dbo)
 		dbo.collection("commands").find().sort(sort_chron).limit(1).toArray((err, res) => {
 			if(err) throw err;
 			if(res.length===undefined || res.length == 0) {
-				//ready = true;
+				ready = true;
 			}
 			else {
-				if(commands.length==0) {ready = true;}
-				else {
 				var options={
 				retain:true,
+				qos:2};
 				qos:0};
-				publish('comm/coords', JSON.stringify(commands[0]), options);
+				publish('comm/coords', res[0], options);
 				var query = {time: res[0].time};
-
-				if(dbo)
-					dbo.collection("commands").deleteOne(query, (err, result) => {
+				dbo.collection("commands").deleteOne(query, (err, result) => {
 					if(err) throw err;
 				});
 				ready = false;
+				//ready = false;
 				commands_complex.shift(); //remove the current position since we reached it
 				path_complex.shift();
-				commands.shift();
 			}
-			}
-		});
-	}
-	else if(topic == 'control/obstacles') {
-		//let ball = {x: ballx, y:bally, color: colors[i-1]};
-		//coord.push(ball);
-		//var myobj = { score: game.cars[i].score, Date: new Date() };
-		//coord.push(message);
-		message=JSON.parse(message);
-		if(dbo) dbo.collection("balls").insertOne(message, function(err, res) {
-			if (err) throw err;
-			console.log("1 ball inserted");
-		});
-		let rad = 1;
-		let ball_compl = math.complex({re:message.x, im:message.y});
-		let newObst = {centre: ball_compl, radius: rad};
-		coord.push(message);
-		[allObstacles, allHitboxes] = addObstacle(newObst, allObstacles);
-		path_complex = [];
-		let originalPath = commands_complex;
-		originalPath.unshift(math.complex({re: rover_coord.x, im: rover_coord.y}));
-		path_complex = pathAdjust(originalPath, allObstacles, allHitboxes, roverWidth, safetyMargin);
-		path_complex.shift(); //remove current position
-	}
-	else if(topic == 'control/positions') {
-		// posx = message.x;
-		//var posy = message.y;
-		var options={
-		retain:true,
-		qos:0};
-		rover_coord = JSON.parse(message);
-		if(commands.length>0 && !ready && rover_coord.x == commands[0].x && rover_coord.y == commands[0].y)
-			publish('ready', "done", options);
-	}
-});
-
-client.on("connect", () => {
-console.log("connected");
-//client.end();
-})
-
-client.on("error", error => {
-console.log("cannot connect " + error);
-process.exit(1)});
-
-
-
-const PORT = process.env.PORT || 8000;
-
-const app = express();
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true })) ;
-
-app.use(cors());
-
-//var jsonParser = bodyParser.json();
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from the server!"});
-});
-var x = 0;
-var y = 0;
-
-app.get("/rover", (req, res) => {
-  x+=1;
-  y+=1;
-  //console.log("rover pos request received");
-	var roverx = x%100-50;
-	var rovery = y%100-50;
-  //res.send("{\"x\": " + roverx + ", \"y\": " + rovery + "}");
-	res.send(rover_coord);
-})
-app.get("/clearnotif", (req, res) => {
-  notifications=[];
-  //console.log("rover pos request received");
-  res.send(notifications);
-})
-var i = 0;
-var ballx, bally;
-let coord = [];
-let colors = ["red", "blue", "yellow", "pink", "green"];
-app.get("/balls", (req, res) => {
-  i++;
-  //console.log("ball request received");
-  if(i<=5) {
-  ballx = Math.random()*100-50;
-  bally = Math.random()*100-50;
-  let ball = {x: ballx, y:bally, color: colors[i-1]};
-  coord.push(ball);
-	notifications.unshift("ball found: " + colors[i-1]);
-	//var myobj = { score: game.cars[i].score, Date: new Date() };
-	if(dbo) dbo.collection("balls").insertOne(ball, function(err, res) {
-		if (err) throw err;
-		console.log("1 ball inserted");
-
-	});
-
-	}
-	if(dbo) dbo.collection("balls").find({}).toArray((err,result) => {
-		if(err) throw err;
-		//console.log("balls read");
-		//coord = result;
-		//res.send(coord);
-	});
-  res.send({balls:coord, dest: commands});
-})
-
-app.post("/clearmap", (req, res) => {
-	console.log(req.body);
-	coord = [];
-	commands = [];
-	if(dbo) dbo.collection("balls").deleteMany({});
-	if(dbo) dbo.collection("commands").deleteMany({});
-	notifications.unshift("map cleared");
-})
-
-/*app.post("/test", (req, res) => {
-	notifications.push(JSON.stringify(req.body));
+@@ -199,7 +199,7 @@ app.post("/clearmap", (req, res) => {
 })*/
 var options={
 retain:true,
+qos:2};
 qos:0};
 
 function toXY(z) {
 	return {x: z.re, y:z.im};
-}
-
+@@ -208,7 +208,7 @@ function toXY(z) {
 app.post('/sendInfo', (req, res) => {
 		if(ready) {
     //publish('comm/coords', req.body.x + '|' + req.body.y, options);
 		ready = false;
+		//ready = false;
 		//console.log(JSON.stringify(rover_coord));
-		let start = math.complex({re: rover_coord.x, im: rover_coord.y});
-		let end = math.complex({re: req.body.x, im: req.body.y});
+		let start = math.complex(rover_coord.x, rover_coord.y);
+		let end = math.complex(req.body.x, req.body.y);
 		commands_complex.push(end);
 		commands.push(req.body);
 		//console.log(commands);
@@ -237,7 +104,6 @@ app.post('/sendInfo', (req, res) => {
 			if(dbo) dbo.collection("commands").insertOne({x: originalPath[i].x, y: originalPath[i].y, time: d.getTime()}, (err, result) => {
 				if(err) throw err;
 				console.log("command saved in db");
-
 			})
 		}*/
 		if(req.body.y && req.body.x)
@@ -247,7 +113,7 @@ app.post('/sendInfo', (req, res) => {
 			var d = new Date;
 			var command = {x: req.body.x, y: req.body.y, time: d.getTime()};
 			let start = commands_complex[commands_complex.length - 2];
-			let end = math.complex(req.body.x, req.body.y);
+			let end = math.complex({re: req.body.x, im: req.body.y);
 			let originalPath = [start, end];
 			commands_complex.push(end);
 			commands.push(req.body);
@@ -261,11 +127,9 @@ app.post('/sendInfo', (req, res) => {
     res.set('Content-Type', 'text/plain');
     res.send(`You sent: req to Express`);
 });
-
 app.get("/notifications", (req, res) => {
 	res.send(notifications);
 });
-
 app.post('/login', (req, res) => {
 	console.log(JSON.stringify(req.body));
 	if(req.body.password == "password")
@@ -277,8 +141,6 @@ app.post('/login', (req, res) => {
 app.listen(PORT, () => {
   console.log("Browser server listening on " + PORT);
 })
-
-
 var message="test message";
 var topic_list=["topic2","topic3","topic4"];
 var topic_o={"topic22":0,"topic33":1,"topic44":1};
@@ -290,7 +152,6 @@ client.subscribe("control/esptest");
 client.subscribe("ready");
 client.subscribe("control/obstacles");
 client.subscribe('control/positions');
-
 var timer_id=setInterval(function(){publish("comm/connection",message,options);},5000);
 //notice this is printed even before we connect
 console.log("end of script");
